d93047c8e775e6c5503a874fec3669118233a564
renxin <renxin714@outlook.com>
2024-09-12 23:27:24 +0800

    add opengl-createWindow

---
+++ openglPractice/createWindow/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+#include "glad/glad.h" // must before glfw3.h
+#include "GLFW/glfw3.h"
+
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height);
+
+void processInput(GLFWwindow *window);
+
+int main()
+{
+    std::cout << "hello window" << std::endl;
+
+    // init
+    glfwInit();
+
+    // use opengl 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // use core mode
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // create window
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello Window", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cerr << "Failed to create window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
